9fb726e8c80847bbf0a6f275d93925ab178d8213
EssieFir <65592600+EssieFir@users.noreply.github.com>
2024-07-30 20:10:31 +1000

    Implement Youtube Iframe API

--- src/views/Player.tsx
+++ src/views/Player.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const Player: React.FC = () => {
-    return (
+    const iframeRef = useRef<HTMLIFrameElement>(null)
+    const playerRef = useRef<YT.Player|null>(null)
 
+    useEffect(() => {
+        const tag = document.createElement('script');
+        tag.src = 'https://www.youtube.com/iframe_api';
+        const firstScriptTag = document.getElementsByTagName('script')[0];
+        if (firstScriptTag) {
+            firstScriptTag.parentNode?.insertBefore(tag, firstScriptTag);
+        }
+        (window as any).onYouTubeIframeAPIReady = () => {
+            if (iframeRef.current) {
+                playerRef.current = new YT.Player(iframeRef.current, {events: {onReady: OnPlayerReady}});
+            }
+        }
+    }, []);
+
+    const OnPlayerReady = (event: YT.PlayerEvent) => {
+        console.log("funny :)");
+        console.log(playerRef.current.getVideoUrl() + "TEST");
+    }
+
+    return (
         <div className="w-full max-h-screen aspect-video overflow-hidden">
             <iframe
-                src="https://www.youtube.com/embed/7lwkojqTtO0?si=UmmWMasQPfasO91Q"
+                ref={iframeRef}
+                src="https://www.youtube.com/embed/7lwkojqTtO0?enablejsapi=1"
                 title="YouTube video player"
                 frameBorder={0}
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -14,7 +36,6 @@ const Player: React.FC = () => {
                 className="w-full h-full"
             ></iframe>
         </div>
-
     );
 };
 
