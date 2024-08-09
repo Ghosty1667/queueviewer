0eabaa15c051c260e1fdf526dac333d4e12c5065
Kieran Price <28439844+Ghosty1667@users.noreply.github.com>
2024-08-09 13:27:17 +1000

    chore: Refactor SideBar component to improve responsiveness and adjusted search functionality

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -9,7 +9,7 @@ interface SideBarProps {
 
 
 const SideBar: React.FC<SideBarProps> = ({ children, sendEvent }) => {
-    const [isOpen, setIsOpen] = React.useState(false);
+    const [isOpen, setIsOpen] = React.useState(true);
 
     const toggleSidebar = () => {
         setIsOpen(!isOpen);
@@ -27,21 +27,25 @@ const SideBar: React.FC<SideBarProps> = ({ children, sendEvent }) => {
 
 
     return (
-        <div className="flex flex-col h-screen">
-            <div className={`bg-gray-700 text-white ${isOpen ? 'w-64' : 'w-22'} flex flex-col h-full`}>
+        <div className={`bg-gray-700 text-white ${isOpen ? 'w-96' : 'w-16'} transition-all duration-200`}>
+            <div className="flex flex-col h-screen">
                 <div className="flex items-center justify-between p-4">
-                    <h1 className="text-lg font-semibold">Up next</h1>
+                    <h1 className={`text-lg font-semibold  ${isOpen ? '' : 'hidden'}`}>Up next</h1>
                     <i onClick={toggleSidebar} className="bi bi-chevron-double-right text-white text-2xl" />
                 </div>
-                <ul className="flex-1 overflow-y-auto">
-                    {children}
-                </ul>
-                <div className="bg-gray-800 p-2 sticky bottom-0 flex items-center">
-                    <input type="text" placeholder="Search" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
-                    <button onClick={handleAddItem} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Add
-                    </button>
-                </div>
+                {isOpen && (
+                    <>
+                        <ul className="flex-1 overflow-y-auto">
+                            {children}
+                        </ul>
+                        <div className="bg-gray-800 p-2 sticky bottom-0 flex items-center">
+                            <input type="text" placeholder="Search" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                            <button onClick={handleAddItem} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                Add
+                            </button>
+
+                        </div>
+                    </>)}
             </div>
         </div>
     );
